f7252377b7491c9942dca043f0faa6add294d044
fabasoad <fabasoad@gmail.com>
2026-01-25 22:24:58 +0900

    fix: go install command 4

--- src/hooks/asdf/pre.sh
+++ src/hooks/asdf/pre.sh
@@ -18,7 +18,12 @@ main() {
     elif command -v zypper >/dev/null 2>&1; then
       zypper install asdf
     elif command -v go >/dev/null 2>&1; then
-      sudo apt install -y git bash
+      if ! command -v git >/dev/null 2>&1; then
+        sudo apt install -y git
+      fi
+      if ! command -v bash >/dev/null 2>&1; then
+        sudo apt install -y bash
+      fi
       go install github.com/asdf-vm/asdf/cmd/asdf@latest
       echo "$(go env GOPATH)/bin" >> "$GITHUB_PATH"
     else
